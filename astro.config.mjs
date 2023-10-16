916f9239aae9e49034db2be6f5b33e13e6136a16
htdanganh <htdanganh@gmail.com>
2023-10-16 02:31:11 -0400

    Updated config file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
+
 export default defineConfig({
     site: 'https://htdanganh.github.io',
-    base: '/htdanganh.github.io/',
 });
