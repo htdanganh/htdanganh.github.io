0b88756051cd16ed05cf5b6f9246542a93feffd0
htdanganh <htdanganh@gmail.com>
2023-10-19 15:39:01 -0400

    Modified images to be stored on AWS S3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
 
+const AWS = require('aws-sdk');
+
 
 export default defineConfig({
     site: 'https://anhhtdang.com',
@@ -8,3 +10,9 @@ export default defineConfig({
 images: {
     minimumCacheTTL: 31536000
 };
+
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: us-east-2
+  });
