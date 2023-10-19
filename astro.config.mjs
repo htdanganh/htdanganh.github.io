97bcd3007cc4aaa8568ba2879564c40100b58044
htdanganh <htdanganh@gmail.com>
2023-10-19 16:18:30 -0400

    Imported AWS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
 import { defineConfig } from 'astro/config';
+import AWS from 'aws-sdk';
 
 
 export default defineConfig({
     site: 'https://anhhtdang.com',
+});
+
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: us-east-2
 });
