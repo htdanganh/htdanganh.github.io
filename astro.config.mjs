fe9d420d25a03946d472bd3c372ab547376991c5
htdanganh <htdanganh@gmail.com>
2023-10-19 16:19:41 -0400

    Fixed typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,5 @@ export default defineConfig({
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: us-east-2
+  region: 'us-east-2'
 });
