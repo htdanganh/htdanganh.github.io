179be0f9b80f950e790626a2c19ae11597f8a05e
htdanganh <htdanganh@gmail.com>
2023-10-23 00:50:49 -0400

    Compressed and updated images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
-import AWS from 'aws-sdk';
-
+import { S3Client } from '@aws-sdk/client-s3'
 
 export default defineConfig({
     site: 'https://anhhtdang.com',
 });
 
-
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: 'us-east-2'
-});
+export const s3 = new S3Client({
+  region: 'us-east-2',
+  credentials: {
+    'accessKeyId': process.env.AWS_ACCESS_KEY_ID,
+    'secretAccessKey': process.env.AWS_SECRET_ACCESS_KEY,
+  },
+})
