c6b2f3944eceeb451e50ce2169b2c38d7c2ee75d
Anh Dang <anhhtdang@gmail.com>
2024-02-17 18:16:51 -0500

    Edited RSS

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,7 +5,7 @@ const matter = require("gray-matter");
 
 async function generate() {
   const feed = new RSS({
-    title: "Your Name",
+    title: "Anh Dang",
     site_url: "https://yoursite.com",
     feed_url: "https://yoursite.com/feed.xml",
   });
