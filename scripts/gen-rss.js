330bccbf266db5601c9f30cb592cad589763cec4
Anh Dang <anhhtdang@gmail.com>
2024-02-17 18:43:47 -0500

    Updated site URL for RSS feed

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -6,8 +6,8 @@ const matter = require("gray-matter");
 async function generate() {
   const feed = new RSS({
     title: "Anh Dang",
-    site_url: "https://yoursite.com",
-    feed_url: "https://yoursite.com/feed.xml",
+    site_url: "https://htdanganh.github.io",
+    feed_url: "https://htdanganh.github.io/feed.xml",
   });
 
   const posts = await fs.readdir(path.join(__dirname, "..", "pages", "posts"));
